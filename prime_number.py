e9501ea52ffaae8a3b464a89a0af17cea915f088
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-08-20 13:16:18 +0530

    # import math # n=int(input()) # c=0 # for i in range(2,int(math.sqrt(n))+1): #     if n%i==0: #         c+=1 # if c==0: #     print("prime") # else: #     print("not a Prime")
    import math
    
    def primeCheck(x):
        sta = 1
        for i in range(2,int(math.sqrt(x))+1): # range[2,sqrt(num)]
            if(x%i==0):
                sta=0
                print("not a prime")
                break
            else:
                continue
        if(sta==1):
            print("prime")
            return sta
    
    num = int(input())
    ret = primeCheck(num)

--- prime_number.py
+++ prime_number.py
@@ -1,8 +1,28 @@
-def p(n):
-    if n<=1:
-        return 'not a prime'
-    for i in range(2,n//2):
-        if n%i==0:
-            return 'not a prime'
-    return 'prime'
-print(p(int(input())))
+# import math
+# n=int(input())
+# c=0
+# for i in range(2,int(math.sqrt(n))+1):
+#     if n%i==0:
+#         c+=1
+# if c==0:
+#     print("prime")
+# else:
+#     print("not a Prime")
+
+import math
+
+def primeCheck(x):
+    sta = 1
+    for i in range(2,int(math.sqrt(x))+1): # range[2,sqrt(num)]
+        if(x%i==0):
+            sta=0
+            print("not a prime")
+            break
+        else:
+            continue
+    if(sta==1):
+        print("prime")
+        return sta
+
+num = int(input())
+ret = primeCheck(num)
