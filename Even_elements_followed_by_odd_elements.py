a9818f1bfd3f7d60494013a71b2e98f97074e098
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-08-26 12:27:46 +0530

    n = int(input()) a = list(map(int,input().split())) ar=[] arr=[] for i in range(n):     if a[i]%2==0:         ar.append(a[i])     else:         arr.append(a[i]) print(*(ar+arr))

---
+++ Even_elements_followed_by_odd_elements.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = list(map(int,input().split()))
+ar=[]
+arr=[]
+for i in range(n):
+    if a[i]%2==0:
+        ar.append(a[i])
+    else:
+        arr.append(a[i])
+print(*(ar+arr))
